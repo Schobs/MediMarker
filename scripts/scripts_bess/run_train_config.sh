a6b502d46c28de2585bd55a6b4309c8a7ff54c7f
Schobs <lawrenceschobs@gmail.com>
2023-01-26 11:49:24 +0000

    changed req file

--- scripts/scripts_bess/run_train_config.sh
+++ scripts/scripts_bess/run_train_config.sh
@@ -6,7 +6,7 @@ module load Anaconda3/5.3.0
 # module load libs/cudnn/7.5.0.56/binary-cuda-10.0.130
 module load cuDNN/7.6.4.38-gcccuda-2019b
 
-source activate test_env
+source activate gp_lannunet
 
 python ../../main.py  "$@" 
 
