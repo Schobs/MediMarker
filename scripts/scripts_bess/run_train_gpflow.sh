cec3cacbc54555194d39c118c7f1daac18efff31
Lawrence Schobs <acq19las@bessemer-node026.shef.ac.uk>
2023-02-27 17:36:26 +0000

    added extra configs

--- scripts/scripts_bess/run_train_gpflow.sh
+++ scripts/scripts_bess/run_train_gpflow.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
+#mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+# echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 
 module load Anaconda3/5.3.0
 
 # module load libs/cudnn/7.5.0.56/binary-cuda-10.0.130
 
-module load cuDNN/7.6.4.38-gcccuda-2019b
+# # module load cuDNN/7.6.4.38-gcccuda-2019b
+# source activate tensorflowEnv
 
-source activate tensorflowEnv
+# module load cuDNN/8.0.4.30-CUDA-11.1.1 
+module load cuDNN/8.0.4.30-CUDA-11.0.2
+source activate tf
+
+# source activate tensorflowEnv
 
 
 python ../../main.py  "$@" 
