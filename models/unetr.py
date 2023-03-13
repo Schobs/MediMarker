51c8c247dab53b1975daac7e5b6a8a42eae85d74
oscargavin <oscarfgavin@gmail.com>
2023-03-13 12:20:26 +0000

    fixed image_size naming issue

--- models/unetr.py
+++ models/unetr.py
@@ -220,26 +220,26 @@ class Transformer(nn.Module):
 
 
 class UNETR(nn.Module):
-    def __init__(self, img_shape=(512, 512), input_dim=1, output_dim=1, embed_dim=768, patch_size=16, num_heads=12, dropout=0.1):
+    def __init__(self, image_size=(512, 512), input_dim=1, output_dim=1, embed_dim=768, patch_size=16, num_heads=12, dropout=0.1):
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.embed_dim = embed_dim
-        self.img_shape = img_shape
+        self.image_size = image_size
         self.patch_size = patch_size
         self.num_heads = num_heads
         self.dropout = dropout
         self.num_layers = 12
         self.ext_layers = [3, 6, 9, 12]
 
-        self.patch_dim = [int(x / patch_size) for x in img_shape]
+        self.patch_dim = [int(x / patch_size) for x in image_size]
 
         # Transformer Encoder
         self.transformer = \
             Transformer(
                 input_dim,
                 embed_dim,
-                img_shape,
+                image_size,
                 patch_size,
                 num_heads,
                 self.num_layers,
