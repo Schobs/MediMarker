e2aa19d8ec4e1ad6472fb7dda9557a31aed46f6b
oscargavin <oscarfgavin@gmail.com>
2023-03-27 15:31:28 +0100

    edited config

--- scripts/scripts_bess/run_train_config_oscar.sh
+++ scripts/scripts_bess/run_train_config_oscar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=gpu
-#SBATCH --qos=gpu
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-
+#SBATCH --mem=32G
+#SBATCH --mail-user=ogavin1@sheffield.ac.uk
+#SBATCH --mail-type=FAIL
+# Rename the job
+#SBATCH --comment=unetr_model_test
 
 module load Anaconda3/5.3.0
 
@@ -12,7 +12,4 @@ module load cuDNN/7.6.4.38-gcccuda-2019b
 
 source activate my_env
 
-python ../../main.py --cfg configs/configs_BSC_projects/ceph_oscar.yaml 
-
-
-
+python ../../main.py --cfg configs/configs_BSC_projects/ceph_oscar.yaml
