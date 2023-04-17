c5d95aa5e8d4049b0d194a8515ec2f9bf89d64fe
oscargavin <oscarfgavin@gmail.com>
2023-04-17 12:32:16 +0100

    potential fix

--- datasets/dataset_index.py
+++ datasets/dataset_index.py
@@ -1,13 +1,13 @@
 """This holds dictionary to various datasets. when adding a new dataset, add it to this dictionary!"""
 
 
-
 from datasets.dataset_generic import DatasetGeneric
 from datasets.dataset_aspire import DatasetAspire
+from datasets.dataset_io import DatasetIO
 
 
 DATASET_INDEX = {
     'generic': DatasetGeneric,
-    'aspire': DatasetAspire
-
+    'aspire': DatasetAspire,
+    'torchio': DatasetIO,
 }
