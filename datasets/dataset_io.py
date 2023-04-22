72de8b35c25220d678b09cdc1dd4c3013c24236d
oscargavin <oscarfgavin@gmail.com>
2023-04-22 14:31:45 +0100

    test

--- datasets/dataset_io.py
+++ datasets/dataset_io.py
@@ -414,12 +414,7 @@ class DatasetIO(ABC, metaclass=DatasetMeta):
                 0).numpy()
             transformed_indicator = transformed_subject["landmark_indicators"].data.numpy(
             )
-            # Remove the extra dimension
-            if transformed_indicator.shape[1] == 1:
-                transformed_indicator = transformed_indicator.squeeze(1)
-            else:
-                print("Warning: Extra dimension has a size different than one. Shape:",
-                      transformed_indicator.shape)
+            transformed_indicator = transformed_indicator[0, :, :, :, :]
 
             # Get the new coordinates from the transformed_indicator
             intermediate_max = np.argmax(transformed_indicator, axis=2)
