1088082909eb6bf7fc490b6df8e2bcafe73457ae
oscargavin <oscarfgavin@gmail.com>
2023-04-27 12:32:08 +0100

    wil this fix

--- datasets/dataset_generic.py
+++ datasets/dataset_generic.py
@@ -7,7 +7,6 @@ from datasets.dataset_base import DatasetBase
 class DatasetGeneric(DatasetBase):
     """
     A custom dataset superclass for loading landmark localization data
-
     Args:
         name (str): Dataset name.
         split (str): Data split type (train, valid or test).
@@ -15,8 +14,6 @@ class DatasetGeneric(DatasetBase):
         annotation_path (str): local directory to file path with annotations.
         annotation_set (str): which set of annotations to use [junior, senior, challenge] (default: "junior")
         image_modality (str): Modality of image (default: "CMRI").
-
-
     References:
         #TO DO
     """
@@ -27,8 +24,3 @@ class DatasetGeneric(DatasetBase):
 
         # super(DatasetBase, self).__init__()
         super(DatasetGeneric, self).__init__(**kwargs)
-
-    def add_additional_sample_attributes(self, data):
-
-        # Extended dataset class can add more attributes to each sample here
-        return data
